e83a87800fc56c059b13b2f40f37bc433692b020
Tal Rasha <524948250@qq.com>
2026-02-05 15:56:02 +0000

    upload coverage

--- test/main.cxx
+++ test/main.cxx
@@ -1,7 +1,10 @@
 #include "ltcd/array/array_partition_test.hxx"
 
+#include <iostream>
+
 int main(int argc, char ** argv)
 {
     TestArrayPartition().execute();
+    std::cout << "Test Done." << std::endl;
     return 0;
 }
