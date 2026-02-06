202cfecf68dfa94b9362dd212edff298086a43bd
Tal Rasha <524948250@qq.com>
2026-02-06 15:18:07 +0000

    add empty array case

--- test/ltcd/array/array_partition_test.cxx
+++ test/ltcd/array/array_partition_test.cxx
@@ -5,6 +5,7 @@
 void TestArrayPartition::execute()
 {
     normalCase();
+    emptyArrayCase();
 }
 
 void TestArrayPartition::normalCase()
@@ -12,4 +13,11 @@ void TestArrayPartition::normalCase()
     int array[9] = {7, 12, 5, 7, 8, 3, 1, 4, 2};
     ArrayPartition().solution(9, array, 9);
     assert(array[0] == 7);
+}
+
+void TestArrayPartition::emptyArrayCase()
+{
+    int * array = nullptr;
+    ArrayPartition().solution(0, array, 0);
+    assert(array == nullptr);
 }
