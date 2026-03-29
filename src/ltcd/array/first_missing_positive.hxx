d44244fb7c342dca676d818386fdd6b6bfb5c176
TalRasha <524948250@qq.com>
2026-03-29 15:08:08 +0800

    fix compile issue 2

--- src/ltcd/array/first_missing_positive.hxx
+++ src/ltcd/array/first_missing_positive.hxx
@@ -1,7 +1,6 @@
 #ifndef FIRST_MISSING_POSITIVE_HXX
 #define FIRST_MISSING_POSITIVE_HXX
 
-#include <ltcd/array/first_missing_positive.hxx>
 #include <vector>
 
 class FirstMissingPositive
--- test/ltcd/array/first_missing_positive_test.hxx
+++ test/ltcd/array/first_missing_positive_test.hxx
@@ -1,6 +1,8 @@
 #ifndef FIRST_MISSING_POSITIVE_TEST_HXX
 #define FIRST_MISSING_POSITIVE_TEST_HXX
 
+#include <ltcd/array/first_missing_positive.hxx>
+
 class TestFirstMissingPositive
 {
 public:
